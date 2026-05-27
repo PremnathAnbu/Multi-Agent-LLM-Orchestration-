eb4c0e1ee293b28e9291497fa69cfceb120e94f9
unknown love <premnath97100@gmail.com>
2026-05-27 09:16:38 +0530

    Initial commit

---
+++ app/backend/api.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI,HTTPException
+from pydantic import BaseModel
+from typing import List,TypedDict
+from app.core.ai_agent import get_resoponse_from_ai_agents
+from app.config.settings import settings
+from app.common.logger import get_logger
+from app.common.custom_exception import CustomException
+
+logger=get_logger(__name__)
+
+app=FastAPI(title="MULTI AI AGENT")
+
+class RequestState(BaseModel):
+    model_name:str
+    system_prompt:str
+    messages:List[str]
+    allow_search:bool
+    
+
+@app.post("/chat")
+def chat_endpoint(request:RequestState):
+    logger.info(f"Received request for model: {request.model_name}")
+    if request.model_name not in settings.ALLOWED_MODEL_NAMES:
+        logger.warning("Invalid model name")
+        raise HTTPException(status_code=400, detail="Invalid model name")
+    try:
+        response=get_resoponse_from_ai_agents(
+            request.model_name,
+            request.messages,
+            request.allow_search,
+            request.system_prompt
+        )
+        logger.info(f"Successfully got response from ai agent {request.model_name}")
+        return {"response":response}    
+    except Exception as e:
+        logger.error("some error occured during response generation")
+        raise HTTPException(status_code=500,
+                            detail=str(CustomException("Failed to get AI respose",error_detail=e)),
+                            )
+    
